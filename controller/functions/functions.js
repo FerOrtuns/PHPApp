85824a4bf0370edfb210ca44f12189ed4a3a8776
FerOrtuns <fernando.ortunolopez@gmail.com>
2023-05-09 10:59:27 +0200

    welcome function added

---
+++ controller/functions/functions.js
@@ -0,0 +1,5 @@
+
+function welcome (){
+
+    alert ('Hola, Bienvenido a la aplicacion de PHP');
+}
--- view/index.php
+++ view/index.php
@@ -4,10 +4,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyPHPApp</title>
-    <link href="/view/css/styles.css" rel="stylesheet" type="text/css">
+    <link href="../view/css/styles.css" rel="stylesheet" type="text/css">
+    <script src="../controller/functions/functions.js"></script>
 </head>
 <body>
 
+<h1>PHP app solutions</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, commodi accusamus? Modi porro, officia distinctio doloribus facilis nisi, velit nemo consequuntur voluptas dignissimos inventore ducimus voluptatum quia eaque odio repellendus!</p>
+<br>
+<input type="button" value="continue" onClick="welcome()" />
 <?php
 
 
